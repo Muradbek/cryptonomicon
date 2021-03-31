1a23f235f21e6f379813dc3776cca7e785c20979
murad <ibrmur89@gmail.com>
2021-03-31 17:47:58 +0300

    e

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                 <div class="flex">
                     <div class="max-w-xs">
                         <label  for="wallet" class="block text-sm font-medium text-gray-700"
-                        >Тикер</label
+                        >Ticker</label
                         >
                         <div class="mt-1 relative rounded-md shadow-md">
                             <input
@@ -37,7 +37,7 @@
               {{coin}}
             </span>
                         </div>
-                        <div v-if="err" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+                        <div v-if="err" class="text-sm text-red-600">This ticker has already been added</div>
                     </div>
                 </div>
                 <button
@@ -58,10 +58,13 @@
                                 d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
                         ></path>
                     </svg>
-                    Добавить
+                    Add
                 </button>
             </section>
             <template v-if="tickers.length">
+                <p>Filter<input v-model="filter" class="mx-4">{{filter}}
+                    <button class="mx-4 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Back</button>
+                    <button class = "mx-4 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Forward</button></p>
                 <hr  class="w-full border-t border-gray-600 my-4" />
                 <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                     <div
@@ -97,7 +100,7 @@
                                         fill-rule="evenodd"
                                         d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                         clip-rule="evenodd"
-                                ></path></svg>Удалить
+                                ></path></svg>Delete
                         </button>
                     </div>
 
@@ -175,7 +178,6 @@
 
     export default {
         name: 'App',
-
         data() {
             return {
                 tickers: [],
@@ -185,7 +187,9 @@
                 autoCoins: [],
                 err: null,
                 coinList: [],
-                spinner: true
+                spinner: true,
+                page: 1,
+                filter: ""
             }
         },
 
@@ -205,6 +209,7 @@
                 this.tickers = JSON.parse(tickersItem)
                 this.tickers.forEach( t => this.subscribeToUpdates(t.name))
             }
+
 
 
             },
@@ -276,8 +281,8 @@
                 },
                 addHint(e) {
                     setTimeout(() => {
-                        if(!e.key == "Enter") this.err = false
-
+                        if(!(e.key == "Enter")) this.err = false
+                        window.err = this.err
 
                         this.autoCoins = []
                         if(this.ticker.length<1) {
